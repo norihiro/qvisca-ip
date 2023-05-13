55207185e18392f7df8e2dc3cff30dbc99390a46
Norihiro Kamae <norihiro@nagater.net>
2023-05-13 15:35:26 +0900

    Initialize WS2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <QApplication>
 #include "camerawindow.h"
 
+static void initialize()
+{
+#ifdef _WIN32
+    WSADATA wsaData;
+    WSAStartup(2, &wsaData);
+#endif
+}
+
 int main(int argc, char *argv[])
 {
+    initialize();
+
     QApplication a(argc, argv);
     CameraWindow w;
     w.show();
